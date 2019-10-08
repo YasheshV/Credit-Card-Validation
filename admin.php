e206a2bf844c5e038e0f0b304de0a4f6fa9697bc
Sumegha Sawa <34690523+Sumegha-14@users.noreply.github.com>
2019-10-08 23:07:49 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -27,26 +27,31 @@ font-style:bold;
 <input type='text' name='username' id='username'  /><br/><br/>
 <label for='password' >Password :</label>
 <input type='password' name='password' id='password' /><br/><br/>
+<input type='conpassword' name='conpassword' id='conpassword' /><br/><br/>
 <input type='submit' name='Submit' value='Submit' />
 </form>
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-$sql = mysqli_connect("localhost", "root", "","credit_card_database");
- $myusername = $_POST['username'];
- $mypassword = $_POST['password']; 
-      $que = "SELECT * FROM admin WHERE username = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($sql,$que);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	if(is_array($row)) 
+
+	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
-	echo "<script type='text/javascript'>alert('You are logged in as $myusername');</script>"; 
-   header('Location://localhost\adminMainPage.html');
-} 
-	else {
-	 echo "<script type='text/javascript'>alert('INVALID DETAILS');</script>";
-	}
-}  
+		$sql = mysqli_connect("localhost", "root", "","credit_card_database");
+ 		$myusername = $_POST['username'];
+ 		$myconpassword = $_POST['conpassword'];
+	 	$mypassword = $_POST['password'];
+      		if($myconpassword == $mypassword){
+			$que = "SELECT * FROM admin WHERE username = '$myusername' and password = '$mypassword'";
+      			$result = mysqli_query($sql,$que);
+      			$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+			if(is_array($row)) 
+			{
+				echo "<script type='text/javascript'>alert('You are logged in as $myusername');</script>"; 
+   				header('Location://localhost\adminMainPage.html');
+			} 
+			else {
+	 			echo "<script type='text/javascript'>alert('INVALID DETAILS');</script>";
+			}
+      		}
+	}  
 
 ?>
 <br><br>
